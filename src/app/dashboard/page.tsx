bec5ea2972b2671b9aaa384740f97aae2820b4dd
Ahmed Mohamed <am6453395@gmail.com>
2025-10-13 22:06:43 +0300

    sync user with dataabase

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+function dashboard() {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+        </div>
+    )
+}
+export default dashboard;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ClerkProvider from "@/components/clerkProvider";
+import UserSync from "@/components/UserSync";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,6 +38,7 @@ export default function RootLayout({
 >
       <html lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}>
+          <UserSync/>
           {children}
         </body>
       </html>
